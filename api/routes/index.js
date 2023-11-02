20dcc4529d05b343e9a9beb89270aa86f8d7b76f
Julia Ribeiro <jrstudin@gmail.com>
2023-11-02 12:58:18 -0300

    created usuariosRoutes and updated other files

---
+++ api/routes/index.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser')
+ 
+const produto = require('./produtoRoute.js');
+const usuarios = require('./usuariosRoutes.js');
+
+module.exports = app => {
+  app.use(
+    bodyParser.json(),
+    produto,
+    usuarios
+  )
+}
+
